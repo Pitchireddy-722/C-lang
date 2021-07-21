650f4023a4f1bba3e7be5c65a37fc366f7231f2b
Pitchireddy-722 <82049941+Pitchireddy-722@users.noreply.github.com>
2021-07-21 14:27:39 +0530

    Add files via upload

---
+++ section-7/Control Flow.c
@@ -0,0 +1,159 @@
+/* IF Statements */
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int score = 95;
+//    int big;
+//    scanf("%d",&big);
+//
+//if (score > big)
+//{
+//    printf("You WIN!!!");
+//}
+//}
+
+
+/* IF Else */
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int Target = 75;
+//    int Score;
+//    scanf("%d",&Score);
+//
+//if (Score > Target)
+//{
+//    printf("You WIN!!!");
+//} else {
+//    printf("You Lose");
+//}
+// return 0;
+//}
+
+/* Example 2 */
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int num;
+//    scanf("%d",&num);
+//
+//if (num % 2 ==0)
+//{
+//    printf("EVEN");
+//} else {
+//    printf("ODD");
+//}
+// return 0;
+//}
+
+/* Else IF */
+#include <stdio.h>
+
+//int main()
+//{
+//    int num;
+//    printf("Enter Number : ");
+//    scanf("%d",&num);
+//
+//    if(num > 0)
+//    {
+//        printf(" +ve \n");
+//    } else if(num == 0)
+//    {
+//        printf("ZERO\n");
+//    } else
+//    {
+//        printf(" -ve \n");
+//    }
+//
+//    return 0;
+//}
+
+/* Switch Case */
+
+/*
+#include <stdio.h>
+int main()
+{
+    enum weekday {Monday,Tuesday,Wednesday,Thrusday,Friday,Saturday,Sunday};
+
+    enum weekday today;
+//    char today;
+    printf("Enter Day : ");
+    scanf("%c",today);
+
+    switch(today)
+    {
+        case Sunday:
+            printf("Today is Sunday");
+            break;
+
+        case Monday:
+            printf("Today is Monday");
+            break;
+
+        case Tuesday:
+            printf("Today is Tuesday");
+            break;
+        default:
+            printf("Not Correct Day");
+            break;
+    }
+    return 0;
+}
+
+*/
+
+//// Prime
+//
+//int main()
+//
+//{
+//    int n,i;
+//    printf("no: ");
+//    scanf("%d",&n);
+//
+//    for(i=2;i<n;i++)
+//    {
+//        if(n%i==0){
+//            break;
+//        }
+//    }
+//    if(i>n/2){
+//        printf("Prime");
+//    }else{
+//        printf("Not Prime");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
