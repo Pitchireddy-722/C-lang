a7e9fd419ded9b9357ecae7f1078b7afb274414e
Pitchireddy-722 <82049941+Pitchireddy-722@users.noreply.github.com>
2021-07-21 14:22:53 +0530

    Add files via upload

---
+++ arrays/array-examples.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+
+/* Print 1 to 10*/
+
+//int main()
+//{
+//    int num[10];
+//    int i,sum=0;
+//    for(i=0;i<10;++i)
+//    {
+//      num[i]=i+1;
+//      sum = sum+num[i];
+//      printf("Array numbers %d\n",num[i]);
+//    }
+//
+//    printf("Array sum %d\n",sum);
+//
+//    return 0;
+//}
+
+/*  **SUM Of Numbers **  */
+
+//int main()
+//{
+//    int num[10]={1,2,3,4,5,6,7,8,9,10};
+//    int i,sum=0;
+//    for(i=0;i<10;++i)
+//    {
+//      sum = sum+num[i];
+//      printf("Array numbers %d\n",num[i]);
+//    }
+//    printf("\nArray sum %d\n",sum);
+//    return 0;
+//}
+
+
+/*  **SUM Of Numbers **  */
+
+//int main()
+//{
+//    int num[10]={1,2,3,4,5,6,7,8,9,10};
+//    int i,sum=0;
+//    for(i=0;i<10;++i)
+//    {
+//      sum = sum+num[i];
+//      printf("Array numbers %d\n",num[i]);
+//      printf("Array sum %d\n",sum);
+//    }
+//    printf("\nArray sum %d\n",sum);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n[200];
+//    int i;
+//    float sum=0,mean;
+//
+//    for(i=1;i<100;i++)
+//    {
+//        n[i]=100+i;
+//        // printf("%d\n",n[i]);
+//    }
+//    for (i=1; i<100;++i)
+//    {
+//        sum = sum+n[i];
+//       // printf("%d\n",sum);
+//    }
+//    printf("%f\n",sum);
+//    mean = sum/100;
+//    printf("%f \n",mean);
+//
+//    return 0;
+//}
+
+/* acces 2-D array */
+
+//int main()
+//{
+//    int a[4][3]={ {0,1,2},
+//                   {3,4,5},
+//                   {6,7,8},
+//                   {9,10,11},
+//                };
+//    int i,j;
+//
+//    for (i=0 ; i<4 ;++i)
+//    {
+//        for(j=0; j<3;++j)
+//        {
+//            printf("a[%d][%d] = %d\n",i,j,a[i][j]);
+//
+//        }
+//
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
